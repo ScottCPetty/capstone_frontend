1aba4d21c1597d6c30ac7bea314a6c77fe0f77a6
ScottCPetty <scottishcheese13@gmail.com>
2024-07-25 12:15:35 -0400

    game functions

--- React/src/components/Game/Game.jsx
+++ React/src/components/Game/Game.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function Game() {
-  return <div>Game</div>;
-}
+const Game = () => {
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "/game.css";
+    document.head.appendChild(link);
+
+    fetch("/game.html")
+      .then((response) => response.text())
+      .then((data) => {
+        const container = document.getElementById("game-container");
+        container.innerHTML = data;
+
+        // Dynamically load the JS file
+        const script = document.createElement("script");
+        script.src = "/game.js";
+        script.async = true;
+        container.appendChild(script);
+      });
+  }, []);
+
+  return <div id="game-container" />;
+};
+
+export default Game;
